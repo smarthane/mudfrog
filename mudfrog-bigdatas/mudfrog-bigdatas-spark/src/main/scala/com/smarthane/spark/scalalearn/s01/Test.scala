32780c1772e4d225b66583337e9192824460bc41
leeyh.lee <leeyh.lee@zkteco.com>
2018-06-26 16:55:44 +0800

    add spark dev

---
+++ mudfrog-bigdatas/mudfrog-bigdatas-spark/src/main/scala/com/smarthane/spark/scalalearn/s01/Test.scala
@@ -0,0 +1,16 @@
+package com.smarthane.spark.scalalearn.s01
+
+/**
+	* Created with by smarthane-cloud-microservice.
+	*
+	* @author: smarthane
+	* @Date: 2018/6/26 16:44
+	* @Description:
+	*/
+object Test {
+	
+	def main(args: Array[String]): Unit = {
+		println("Hello world ")
+	}
+	
+}
